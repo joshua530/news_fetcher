f6fcf66eaa00e9db3c72f3f80d4f16a9fb7c401d
joshua530 <joshuaomari5@gmail.com>
2022-01-26 09:20:21 +0300

    Wrap http request in function
    Unnecessary headers have been removed.
    Dynamic construction of the query has been added.

--- exp/request.py
+++ exp/request.py
@@ -1,23 +1,20 @@
 import requests
 
-r = requests.get('http://www.duckduckgo.com?q=news&t=h_', headers={
-    # 'Host': 'duckduckgo.com',
-    # 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0',
-    'User-Agent': 'py-bot',
-    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    # 'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    # 'Connection': 'keep-alive',
-    # 'Upgrade-Insecure-Requests': '1',
-    # 'Sec-Fetch-Dest': 'document',
-    # 'Sec-Fetch-Mode': 'navigate',
-    # 'Sec-Fetch-Site': 'none',
-    # 'Sec-Fetch-User': '?1',
-    # 'Referer': 'https://duckduckgo.com'
-})
 
-# print(r.status_code)
-# print(r.headers)
-# print(type(r.headers))
+def get(keywords: list) -> str:
+    if type(keywords).__name__ != 'list' and type(keywords).__name__ != 'tuple':
+        raise TypeError(
+            'Invalid keywords type. Only a tuple or list should be used')
 
-print(r.text)
+    search_query = '+'.join(keywords)
+    url = 'https://www.duckduckgo.com?q={}&t=h_'.format(search_query)
+
+    req = requests.get(url, headers={
+        'User-agent': 'py-bot',
+        'Accept-encoding': 'gzip, deflate, br'
+    })
+    response_text = req.text
+    return response_text
+
+
+get(('news', 'latest', 'agriculture'))
