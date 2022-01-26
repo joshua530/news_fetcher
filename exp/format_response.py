f35718ae4b5475346cad090a1702419cf02e8d11
joshua530 <joshuaomari5@gmail.com>
2022-01-26 22:41:52 +0300

    Add test_result.py

--- exp/format_response.py
+++ exp/format_response.py
@@ -1,3 +1,12 @@
 from request import get
 
 response = get(['news', 'latest', 'entertainment'])
+
+
+def format_response(response_str: str, type: str):
+    '''
+    Formats string obtained as response in the required
+    format:
+
+    Accepted formats are: xml, plain text and YAML
+    '''
---
+++ exp/test_result.py
@@ -0,0 +1,70 @@
+expected = {
+    {
+        'title': 'Fox News - Breaking News Updates | Latest News Headlines ...',
+        'breadcrumb_url': 'www.foxnews.com',
+        'description': 'Fox News official website with news, politics, entertainment, tech, science, health, travel, lifestyle, and sports.',
+        'site_links': [
+            {'name': 'Politics', 'url': 'https://www.foxnews.com/politics'},
+            {'name': 'U.S', 'url': 'https://www.foxnews.com/us'},
+            {'name': 'World', 'url': 'https://www.foxnews.com/world/'},
+            {'name': 'Entertainment', 'url': 'https://www.foxnews.com/entertainment'},
+            {'name': 'Opinion', 'url': 'https://www.foxnews.com/opinion'},
+            {'name': 'Watch Live', 'url': 'https://video.foxnews.com/v/5614615980001/'},
+        ]
+    },
+    {
+        {
+            'title': 'Google News',
+            'breadcrumb_url': 'https://news.google.com',
+            'description': 'We would like to show you a description here but the site won\'t allow us.'
+        },
+
+        {
+            'title': 'NBC News - Breaking News & Top Stories - Latest World, US ...',
+            'breadcrumb_url': 'https://www.nbcnews.com',
+            'description': 'Go to NBCNews.com for breaking news, videos, and the latest top stories in world news, business, politics, health and pop culture.'
+        },
+
+        {
+            'title': 'CNN - Breaking News, Latest News and Videos',
+            'breadcrumb_url': 'https://www.cnn.com',
+            'description': 'View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.'
+        },
+
+        {
+            'title': 'ABC News - Breaking News, Latest News, Headlines & Videos',
+            'breadcrumb_url': 'https://abcnews.go.com',
+            'description': 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com'
+        },
+
+        {
+            'title': 'News: U.S. and World News Headlines : NPR',
+            'breadcrumb_url': 'https://www.npr.org › sections › news',
+            'description': 'NPR news, audio, and podcasts. Coverage of breaking stories, national and world news, politics, business, science, technology, and extended coverage of major national and world events.'
+        },
+
+        {
+            'title': 'CBS News - Breaking news, 24/7 live streaming news & top ...',
+            'breadcrumb_url': 'https://www.cbsnews.com',
+            'description': 'Watch CBSN the live news stream from CBS News and get the latest, breaking news headlines of the day for national news and world news today.'
+        },
+
+        {
+            'title': 'New York Post - Breaking News, Latest US & World Headlines',
+            'breadcrumb_url': 'https://nypost.com › news',
+            'description': 'Read today\'s latest breaking news from the US and around the world on the New York Post. Robert F Kennedy Jr. compared vaccine mandates to Nazi Germany during a rally at the National Mall in ...'
+        },
+
+        {
+            'title': 'MSN',
+            'breadcrumb_url': 'https://www.msn.com › en-us › news',
+            'description': 'MSN'
+        },
+
+        {
+            'title': 'The Latest US and World News - USATODAY.com',
+            'breadcrumb_url': 'https://www.usatoday.com › news',
+            'description': 'Get the latest national, international, and political news at USATODAY.com'
+        },
+    }
+}
