d7ee574f72558806184462020e64a73d2eeb06c2
joshua530 <joshuaomari5@gmail.com>
2022-01-26 23:47:15 +0300

    Rename format_response to parse_response

--- exp/format_response.py
+++ exp/format_response.py
@@ -1,12 +1,42 @@
-from request import get
+# from request import get
+from test_result import expected
+import os
 
-response = get(['news', 'latest', 'entertainment'])
+# response = get(['news', 'latest', 'entertainment'])
 
 
-def format_response(response_str: str, type: str):
+def parse_response(response_str: str, type: str) -> dict:
     '''
-    Formats string obtained as response in the required
-    format:
-
-    Accepted formats are: xml, plain text and YAML
+    Parses a given string into a list of the obtained results with each
+    result in dictionary form
     '''
+    # break down the text into a dictionary(results) of dictionaries(result)
+    # the child dictionaries will contain info in the format
+    # {
+    #   title: <title>,
+    #   breadcrumb_url: <breadcrumb url>,
+    #   description: <result description(get rid of <b> tags)>,
+    #   site_links: [
+    #     {name: <site name>, url: <link url>}
+    #   ]
+    # }
+
+    # format the results in the required format
+    return {}
+
+# Formats string obtained as response in the required
+#     format:
+
+#     Accepted formats are: xml, plain text and YAML
+
+
+test_data = os.path.dirname(os.path.realpath(__file__)) + '/test_data.html'
+with open(test_data) as test_data:
+    contents = test_data.read()
+
+parsed_data = parse_response(contents, 'text')
+
+if parsed_data == expected:
+    print('Yaaay')
+else:
+    print('Nah')
