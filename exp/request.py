cc542bf43225e6eb54b0215e9d38176d455f2d59
joshua530 <joshuaomari5@gmail.com>
2022-01-27 13:21:39 +0300

    Reformat request.py

--- exp/request.py
+++ exp/request.py
@@ -3,16 +3,14 @@ from typing import Union
 
 
 def get(keywords: Union[list, tuple]) -> str:
-    if type(keywords).__name__ != 'list' and type(keywords).__name__ != 'tuple':
-        raise TypeError(
-            'Invalid keywords type. Only a tuple or list should be used')
+    if type(keywords).__name__ != "list" and type(keywords).__name__ != "tuple":
+        raise TypeError("Invalid keywords type. Only a tuple or list should be used")
 
-    search_query = '+'.join(keywords)
-    url = 'https://www.duckduckgo.com?q={}&t=h_'.format(search_query)
+    search_query = "+".join(keywords)
+    url = "https://www.duckduckgo.com?q={}&t=h_".format(search_query)
 
-    req = requests.get(url, headers={
-        'User-agent': 'py-bot',
-        'Accept-encoding': 'gzip, deflate, br'
-    })
+    req = requests.get(
+        url, headers={"User-agent": "py-bot", "Accept-encoding": "gzip, deflate, br"}
+    )
     response_text = req.text
     return response_text
